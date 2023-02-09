63f3b0fb143f8be186d5f3ece191be92b76aecba
David <110458896+henriikkanorra@users.noreply.github.com>
2023-02-09 17:57:08 +0100

    first build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
+    fontFamily: {
+      'header': ['Great Vibes'],
+      'sub': ['Cinzel'],
+      'basic': ['Montserrat']
+    },
     extend: {
       colors: {
         'daily-dev-tips': "#F89283"
